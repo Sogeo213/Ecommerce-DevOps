3433fdd70d7e43902f83b51fe54dd5b5cb7ed8e3
Sogeo213 <sochiohorng22@gmail.com>
2026-06-17 10:37:22 +0700

    Fix: temporarily disable whitenoise to prevent crash

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,8 +29,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # សម្រាប់គ្រប់គ្រង Static Files លើ Railway
+    'django.middleware.security.SecurityMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
